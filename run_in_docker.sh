db55d823e96071038834eab2921ca7cf6495faac
joecom7 <giovanni.francesco11c@gmail.com>
2025-09-19 12:58:03 +0200

    added --build option to docker script

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,11 +2,22 @@
 
 xhost +local:root
 
+DO_BUILD=false
+
+# Controllo argomenti
+if [[ "$1" == "--build" ]]; then
+    DO_BUILD=true
+fi
+
 if nvidia-smi &>/dev/null; then
-    docker compose -f ./docker/docker-compose_gpu.yml build
+    if $DO_BUILD; then
+        docker compose -f ./docker/docker-compose_gpu.yml build
+    fi
     docker compose -f ./docker/docker-compose_gpu.yml up
 else
-    docker compose -f ./docker/docker-compose.yml build
+    if $DO_BUILD; then
+        docker compose -f ./docker/docker-compose.yml build
+    fi
     docker compose -f ./docker/docker-compose.yml up
 fi
 
